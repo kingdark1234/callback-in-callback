03c6c4afc10234d96533335e4af8a76ee8cd3b59
Atul R <atulanand94@gmail.com>
2017-11-21 11:39:34 +0700

    Done with server changes

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.get('/login', (req, res) => res.send({token:'f6eb4fb963373658b32251dbd045bf24'}));
+app.get('/login', (req, res) => res.send('f6eb4fb963373658b32251dbd045bf24'));
 
 app.get('/user', (req, res) => {
-  if(req.query && (req.query.token === 'f6eb4fb963373658b32251dbd045bf24')){
-    res.send({name:'Test User', dateOfBirth: '24-02-1970'});
-  }else{
-    res.status(400).send({error:'FAIL NO TOKEN PRESENT'});
+  if(!req.query){
+    res.status(400).send('FAIL NO TOKEN PRESENT');
+  } else {
+    if(req.query.token === 'f6eb4fb963373658b32251dbd045bf24'){
+      res.send('24-02-1970');
+    }
+    else{
+      res.status(400).send('FAIL WRONG TOKEN SENT');
+    }
   }
 });
 
